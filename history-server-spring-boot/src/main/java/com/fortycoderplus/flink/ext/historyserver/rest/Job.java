146dc25de4e0d61db24037239f431e42cebf8e1e
Codingendless <codingendless@40coderplus.com>
2023-01-20 21:52:09 +0800

    add interface to Flink History Server Rest API

---
+++ history-server-spring-boot/src/main/java/com/fortycoderplus/flink/ext/historyserver/rest/Job.java
@@ -0,0 +1,63 @@
+/*
+ * (c) Copyright 2023 40CoderPlus. All rights reserved.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fortycoderplus.flink.ext.historyserver.rest;
+
+import lombok.Builder;
+import lombok.Data;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonProperty;
+
+@Builder
+@Data
+public class Job {
+
+    private String jid;
+    private String name;
+    private String state;
+
+    @JsonProperty("start-time")
+    private int startTime;
+
+    @JsonProperty("end-time")
+    private int endTime;
+
+    private int duration;
+
+    @JsonProperty("last-modification")
+    private int lastModification;
+
+    private Tasks tasks;
+
+    @Builder
+    @Data
+    public static class Tasks {
+        private int total;
+        private int created;
+        private int scheduled;
+        private int deploying;
+        private int running;
+        private int finished;
+        private int canceling;
+        private int canceled;
+        private int failed;
+        private int reconciling;
+        private int initializing;
+    }
+}
---
+++ history-server-spring-boot/src/main/java/com/fortycoderplus/flink/ext/historyserver/rest/Overview.java
@@ -0,0 +1,56 @@
+/*
+ * (c) Copyright 2023 40CoderPlus. All rights reserved.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fortycoderplus.flink.ext.historyserver.rest;
+
+import lombok.Builder;
+import lombok.Data;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonProperty;
+
+@Builder
+@Data
+public class Overview {
+
+    @JsonProperty("flink-version")
+    private String flinkVersion;
+
+    @JsonProperty("flink-commit")
+    private String flinkCommit;
+
+    private int taskmanagers;
+
+    @JsonProperty("slots-total")
+    private int slotsTotal;
+
+    @JsonProperty("slots-available")
+    private int slotsAvailable;
+
+    @JsonProperty("jobs-running")
+    private int jobsRunning;
+
+    @JsonProperty("jobs-cancelled")
+    private int jobsCancelled;
+
+    @JsonProperty("jobs-failed")
+    private int jobsFailed;
+
+    @JsonProperty("jobs-finished")
+    private int jobsFinished;
+}
