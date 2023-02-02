2fb28ac2a663e33e50d039f104808c9a57f80444
Codingendless <codingendless@40coderplus.com>
2023-02-02 08:18:48 +0800

    support auto redirect to true page

--- history-server-embedded-dashboard/src/main/java/com/fortycoderplus/flink/ext/historyserver/embedded/infrastructure/DashboardWebConfigure.java
+++ history-server-embedded-dashboard/src/main/java/com/fortycoderplus/flink/ext/historyserver/embedded/infrastructure/DashboardWebConfigure.java
@@ -20,15 +20,31 @@
 
 package com.fortycoderplus.flink.ext.historyserver.embedded.infrastructure;
 
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class DashboardWebConfigure implements WebMvcConfigurer {
+public class DashboardWebConfigure
+        implements WebMvcConfigurer, WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations("classpath:/web/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/notFound").setViewName("forward:/index.html");
+    }
+
+    @Override
+    public void customize(ConfigurableServletWebServerFactory factory) {
+        factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/notFound"));
     }
 }
